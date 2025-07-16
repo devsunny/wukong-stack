95386e873724ce4fdac66c59086383c74cc78955
sunny Liu <sunnyliu2@gmail.com>
2025-07-15 21:30:11 -0400

    add generator

--- wukong/backend_generator.py
+++
@@ -1,6 +0,0 @@
-from typing_extensions import List
-from pgsql_parser import Table
-
-
-def create_backend(table_definitions:List[Table]):
-    pass
--- wukong/frontend_generator.py
+++
@@ -1,6 +0,0 @@
-from typing_extensions import List
-from pgsql_parser import Table
-
-
-def create_frontend(table_definitions:List[Table]):
-    pass
